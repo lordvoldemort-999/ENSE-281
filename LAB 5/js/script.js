f5243814d83d1127cf17e0d443b5fee9c780d85a
Abraham Omoregie <abrahamomoregie4uni.abe@gmail.com>
2026-02-20 14:26:25 -0600

    finised adding functionalities

--- LAB 5/js/script.js
+++ LAB 5/js/script.js
@@ -33,43 +33,38 @@ $(document).ready(() => {
 
   let notes = [];
 
-  // let notes = [
-  //   new note(0, "User A", "This is a text written by User A"),
-  //   new note(1, "User B", "This is a text written by User B"),
-  //   new note(2, "User C", "This is a text written by User C"),
-  // ];
-
-  
-
   //----------------------------------------------------------------functions-------------------------------------------------------------------------------
   function loadUserInterface(user) {
     currentUser = user;
     $("#logged-in-as").text(`Logged in as ${user.name}`);
 
-    $("#note-container").empty();
-
+    // $("#note-container").toggleClass("hide", notes.length === 0);
+    $("#list-group").empty();
+    
     $.each(notes, function (index, noteItem) {
       let upActive, downActive;
-
-      if(noteItem.upvoted.includes(currentUser.iD)) {
+      
+      if (noteItem.upvoted.includes(currentUser.iD)) {
         upActive = "active";
-      } else { 
+      } else {
         upActive = "";
       }
-
-      if(noteItem.downvoted.includes(currentUser.iD)) {
+      
+      if (noteItem.downvoted.includes(currentUser.iD)) {
         downActive = "active";
       } else {
         downActive = "";
       }
-
-      // TO DO: change note display form <input> to <span> or <p>
-      $("#note-container").append(
-        $(`
-        <div class = "note-div mb-2 p-2 border rounded">
-          <p style="text-align: center" class="my-1">${noteItem.text}</p>
-          <div>
-              <button type="button" class="btn btn-outline-success me-1 ms-1 upvote ${upActive}" data-id="${noteItem.iD}">
+      
+      const totalVotes = noteItem.upvoted.length - noteItem.downvoted.length;
+      const isOwnNote = noteItem.creator ===currentUser.name;
+      
+      if (!isOwnNote) {
+        $("#list-group").append(
+          $(`
+            <li class="list-group-item border rounded p-2 d-flex align-items-center gap-2 my-1">
+              ${noteItem.text}
+              <button type="button" class="btn btn-outline-success upvote ms-0 ${upActive}" data-id="${noteItem.iD}">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -79,12 +74,12 @@ $(document).ready(() => {
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
-                    d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
+                  fill-rule="evenodd"
+                  d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
                   />
                 </svg>
               </button>
-              <button type="button" class="btn btn-outline-danger me-1 ms-1 downvote ${downActive}" data-id="${noteItem.iD}">
+              <button type="button" class="btn btn-outline-danger downvote ${downActive}" data-id="${noteItem.iD}">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -94,33 +89,45 @@ $(document).ready(() => {
                   viewBox="0 0 16 16"
                 >
                   <path
-                    fill-rule="evenodd"
-                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
+                  fill-rule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
                   />
                 </svg>
-                </button>
-                </div>
-              </div>`),
+              </button>
+              <span>
+                ${totalVotes}
+              </span>
+            </li>
+            `)
+        )
+      } else {
+        $("#list-group").append(
+        $(`
+        <li class="list-group-item border rounded p-2 d-flex align-items-center gap-2 my-1">
+              ${noteItem.text}
+              <span class="border-start p-1">
+                ${totalVotes}
+              </span>
+            </li>`)
       );
+      }
     });
   }
   //----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 
   //-------------------------------------event listeners------------------------------------------------------------------------------------------------------------
   $(document).on("click", ".dropdown-item", function () {
     const selectedId = parseInt($(this).data("id"));
     const selectedUser = users.find((user) => user.iD === selectedId);
-    
+
     if (selectedUser) {
       loadUserInterface(selectedUser);
     }
   });
-  
-  $(document).on("click", ".upvote", function (){
+
+  $(document).on("click", ".upvote", function () {
     const noteId = parseInt($(this).data("id"));
-    const selectedNote = notes.find(n => n.iD === noteId);
+    const selectedNote = notes.find((n) => n.iD === noteId);
 
     if (selectedNote) {
       selectedNote.upvote(currentUser.iD);
@@ -128,9 +135,9 @@ $(document).ready(() => {
     }
   });
 
-  $(document).on("click", ".downvote", function (){
+  $(document).on("click", ".downvote", function () {
     const noteId = parseInt($(this).data("id"));
-    const selectedNote = notes.find(n => n.iD === noteId);
+    const selectedNote = notes.find((n) => n.iD === noteId);
 
     if (selectedNote) {
       selectedNote.downvote(currentUser.iD);
@@ -138,16 +145,11 @@ $(document).ready(() => {
     }
   });
 
-  $(document).on("click", "#upload-btn", function (){
-    
+  $(document).on("click", "#upload-btn", function () {
     const textValue = $("#to-upload").val();
 
     if (textValue.trim() !== "") {
-      const newNote = new note(
-        notes.length,
-        currentUser.name,
-        textValue
-      );
+      const newNote = new note(notes.length, currentUser.name, textValue);
 
       notes.push(newNote);
       $("#to-upload").val("");
@@ -157,7 +159,5 @@ $(document).ready(() => {
   });
   //----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
   loadUserInterface(currentUser);
-
 });
