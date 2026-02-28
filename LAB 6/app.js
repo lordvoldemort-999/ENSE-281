3cc47c41c678d32723a7966b0113b801d87dd8a1
Abraham Omoregie <abrahamomoregie4uni.abe@gmail.com>
2026-02-28 01:52:08 -0600

    implemented functionality

--- LAB 6/app.js
+++ LAB 6/app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true})); 
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -15,6 +16,32 @@ app.get("/pages/todo.html", (req, res) => {
     res.sendFile(__dirname + "/pages/todo.html");
     console.log("Successfully requested and sent the todo page");
 })
+
+app.post("/", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    fs.readFile (__dirname + "/users.json",
+    "utf-8",
+    (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err.message);
+            return;
+        } try {
+            const users = JSON.parse(jsonString);
+            const foundUser = users.find(user => user.email === email && user.password === password);
+            if (foundUser) {
+                res.sendFile(__dirname + "/pages/todo.html")
+                console.log("User:", foundUser.email, "has logged in")
+            } else {
+                res.redirect("/");
+            }
+        } catch (err) {
+            console.log("Error parsing JSON:", err.message);
+        }
+    }
+    ); 
+});
 
 app.listen(port, () => {
     console.log (`Server is running on http://localhost:${port}`);
