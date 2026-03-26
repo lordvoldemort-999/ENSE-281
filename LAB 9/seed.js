1fc746c9cfbdc208a5bdb7203ac42459b233ecc6
Abraham Omoregie <abrahamomoregie4uni.abe@gmail.com>
2026-03-26 00:43:57 -0600

    changed schemas and implemented passport

--- LAB 9/seed.js
+++ LAB 9/seed.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose").default
+const passportLocalMongoose = require("passport-local-mongoose");
 mongoose.connect("mongodb://localhost:27017/notevote");
 
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String,
 });
 
 const postSchema = new mongoose.Schema({
-  _id: String,
   text: String,
   creator: String,
   upvotes: [String],
@@ -17,61 +15,63 @@ const postSchema = new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-const user = mongoose.model("user", userSchema);
-const post = mongoose.model("post", postSchema);
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
 
 async function createDB() {
-    await user.insertMany([
-      { username: "aoo988@uregina.ca", password: "fakepassword@@" },
-      { username: "secondUser@gmail.com", password: "secondUserPassword" },
-    ]);
-    
-    await post.insertMany([
-      {
-        _id: "0",
-        text: "First post by aoo988",
-        creator: "aoo988@uregina.ca",
-        upvotes: [],
-        downvotes: [],
-      },
-      {
-        _id: "1",
-        text: "Second post by aoo988",
-        creator: "aoo988@uregina.ca",
-        upvotes: ["secondUser@gmail.com"],
-        downvotes: [],
-      },
-      {
-        _id: "2",
-        text: "Third post by aoo988",
-        creator: "aoo988@uregina.ca",
-        upvotes: [],
-        downvotes: ["secondUser@gmail.com"],
-      },
-      {
-        _id: "3",
-        text: "First post by secondUser",
-        creator: "secondUser@gmail.com",
-        upvotes: [],
-        downvotes: [],
-      },
-      {
-        _id: "4",
-        text: "Second post by secondUser",
-        creator: "secondUser@gmail.com",
-        upvotes: ["aoo988@uregina.ca"],
-        downvotes: [],
-      },
-      {
-        _id: "5",
-        text: "Third post by secondUser",
-        creator: "secondUser@gmail.com",
-        upvotes: [],
-        downvotes: ["aoo988@uregina.ca"],
-      },
-    ]);
-    
-    mongoose.connection.close();
+  await User.deleteMany({});
+  await Post.deleteMany({});
+
+  await User.register(
+    new User({ username: "aoo988@uregina.ca" }),
+    "fakepassword@@",
+  );
+
+  await User.register(
+    new User({ username: "secondUser@gmail.com" }),
+    "secondUserPassword",
+  );
+
+  await Post.insertMany([
+    {
+      text: "First post by aoo988",
+      creator: "aoo988@uregina.ca",
+      upvotes: [],
+      downvotes: [],
+    },
+    {
+      text: "Second post by aoo988",
+      creator: "aoo988@uregina.ca",
+      upvotes: ["secondUser@gmail.com"],
+      downvotes: [],
+    },
+    {
+      text: "Third post by aoo988",
+      creator: "aoo988@uregina.ca",
+      upvotes: [],
+      downvotes: ["secondUser@gmail.com"],
+    },
+    {
+      text: "First post by secondUser",
+      creator: "secondUser@gmail.com",
+      upvotes: [],
+      downvotes: [],
+    },
+    {
+      text: "Second post by secondUser",
+      creator: "secondUser@gmail.com",
+      upvotes: ["aoo988@uregina.ca"],
+      downvotes: [],
+    },
+    {
+      text: "Third post by secondUser",
+      creator: "secondUser@gmail.com",
+      upvotes: [],
+      downvotes: ["aoo988@uregina.ca"],
+    },
+  ]);
+
+  mongoose.connection.close();
 }
 
 createDB();
